65f1c9e2c7621ce3a753aa6b41ea92bcf3e3a133
pmalen327 <malenpreston@gmail.com>
2023-02-23 12:01:22 -0700

    fixed some sympy/numpy conflicts, still not there though

--- project1sympy.py
+++ project1sympy.py
@@ -3,36 +3,60 @@
 # February 2023
 
 # trying a sympy approach
-
 from sympy import *
 import numpy as np
+from numpy import linalg as la
+from math import *
+
 
 x,h,L = symbols('x h L')
 
+N = 10
+h = N + 1
+j = []
+
+for num in range(0,N+2):
+    j.append(num)
+
+xVec = []
+for num in j:
+    xVec.append(num/(h))
+
 u = x*(x-1)
 
-posDiff = x+h/2
-negDiff = x-h/2
+# the exact f(x) on the RHS
+def f(var):
+    val = 4
+    for L in range(1,6):
+        val += ((sin(L*pi*var)*2) + (2*var-1)*(cos(L*pi*var))*L*pi*var)/h
+    return val
 
-uPos = posDiff*(posDiff-1)
-uNeg = negDiff*(negDiff-1)
+fVec = []
+for i in xVec:
+    fVec.append(f(i))
 
-opD = uPos-uNeg/(h/2)
+# kappaArg = sin(L*pi*x)/(L+1)
+# kappa = 2 + Sum(kappaArg, (L,1,5)).doit()
+
+term1 = (sin(L*pi*(x+h/2))*(x+h/2-1))/(L+1)
+term2 = -(sin(L*pi*(x-h/2))*(x-h/2-1))/(L+1)
+
+sum1 = Sum(term1, (L,1,5)).doit()
+sum2 = Sum(term2, (L,1,5)).doit()
+laplaceOp = 2 + ((sum1-sum2)/h)
 
 
-kappaArg = 1/(L+1)*sin(L*np.pi*x)
-kappa = 2 + Sum(kappaArg, (L,1,5)).doit()
 
-laplaceArg = kappa*opD
-lpPos = laplaceArg + h/2
-lpNeg = laplaceArg - h/2
-opLaplace = lpPos-lpNeg/(h/2)
 
 # now just need to substitute values
-print(opLaplace.subs({x:1, h: 2}))
+sol = []
+for num in xVec:
+    sol.append(laplaceOp.subs(x,num))
+sol = np.array(sol, dtype=np.float64)
 
 
-
+# LTE = abs(la.norm(sol)-la.norm(fVec))
+print(fVec)
 
 
 
