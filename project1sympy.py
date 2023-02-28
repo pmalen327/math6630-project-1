d459ba9bc3d0e9994035ae916f03151f5bbaf814
pmalen327 <malenpreston@gmail.com>
2023-02-28 12:46:43 -0700

    nevermind, fApprox is the issue, using kappa as constant for debugging

--- project1sympy.py
+++ project1sympy.py
@@ -9,14 +9,14 @@ from scipy.linalg import norm
 
 
 N = 10
-h = 1/(N+1)
+h = round(1/(N+1),5)
 j = []
 for int in range(0,N+2):
     j.append(int)
 
 xVec = []
 for int in j:
-    xVec.append(int*h)
+    xVec.append(round(int*h,5))
 
 
 fVec = []
@@ -25,32 +25,35 @@ for x in xVec:
     val2 = 0
     val3 = 0
     for L in range(1,6):
-        val2 += 2*(np.sin(np.radians(L*np.pi*x)))/(L+1)
-        val3 += ((2*x-4)*(np.cos(np.radians(np.pi*L*x)))*np.radians(L*np.pi*x))/(L+1)
+        val2 += 2*(np.sin(L*np.pi*x))/(L+1)
+        val3 += ((2*x-4)*(np.cos(np.pi*L*x))*L*np.pi*x)/(L+1)
     fVec.append(val1+val2+val3)
 
 
 
-# THIS IS THE PROBLEM
-kappaPos = []
-kappaNeg = []
-for x in xVec:
-    valKappaPos = 2
-    valKappaNeg = 2
-    for L in range(1,6):
-        valKappaPos += (np.sin(np.radians(L*np.pi*(x+h/2))))/(L+1)
-        valKappaNeg += (np.sin(np.radians(L*np.pi*(x-h/2))))/(L+1)
-    kappaPos.append(valKappaPos)
-    kappaNeg.append(valKappaNeg)
+kappaPos = [2.3 for i in range(0,len(xVec))]
+kappaNeg = [2 for i in range(0,len(xVec))]
 
+# for x in xVec:
+#     valKappaPos = 2
+#     valKappaNeg = 2
+#     for L in range(1,6):
+#         valKappaPos += (np.sin(L*np.pi*(x+h/2)))/(L+1)
+#         valKappaNeg += (np.sin(L*np.pi*(x+h/2)))/(L+1)
+#     kappaPos.append(valKappaPos)
+#     kappaNeg.append(valKappaNeg)
+
+# THIS IS THE PROBLEM
+# fukkkkkkkkkkkkkkkkk
 fApprox = []
 for index in range(0, len(xVec)):
     val = 0
-    uInit = (index*(x-1))
+    uInit = (index*(index-1))
     uNeg = (index-h)*(index-h-1)
     uPos = (index+h)*(index+h-1)
-    val += (-uInit*(kappaPos[index]+kappaNeg[index])) + (kappaPos[index]*uPos) + (kappaNeg[index]*uNeg)
-    fApprox.append(val/(h**2))
+    val += -(uInit*(kappaPos[index]+kappaNeg[index])) + (kappaPos[index]*uPos) + (kappaNeg[index]*uNeg)
+    fApprox.append(round(val/(h**2),5))
 
 
-print(fApprox,fVec)
+# print(fApprox,fVec)
+print(xVec,fVec,fApprox)
