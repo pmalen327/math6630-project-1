adbfab044855e06fa43f0850a3f1852f3ba8b46a
pmalen327 <malenpreston@gmail.com>
2023-02-27 17:12:38 -0700

    set kappa constant to debug, fVec is the problem child

--- project1sympy.py
+++ project1sympy.py
@@ -7,7 +7,7 @@ from numpy import linalg as la
 from scipy.linalg import norm
 
 
-N = 1000
+N = 10
 h = 1/(N+1)
 j = []
 for int in range(0,N+2):
@@ -18,31 +18,37 @@ for int in j:
     xVec.append(int*h)
 
 
+# THIS IS THE PROBLEM
 fVec = []
 for x in xVec:
-    val = 4
-    for L in range(0,6):
-        val += (((2*x-1)*np.cos(L*np.pi*x)*L*np.pi)+(2*np.sin(L*np.pi*x)))/(L+1)
-    fVec.append(val)
+    val1 = 8
+    val2 = 0
+    val3 = 0
+    for L in range(1,6):
+        val2 += 2*(np.sin(np.radians(L*np.pi*x)))/(L+1)
+        val3 += ((2*x-4)*(np.cos(np.radians(np.pi*L*x)))*L*np.pi*x)/(L+1)
+    fVec.append(val1+val2+val3)
 
-kappaPos = []
-kappaNeg = []
-for x in xVec:
-    valKappaPos = 2
-    valKappaNeg = 2
-    for L in range(0,6):
-        valKappaPos += (np.sin(L*np.pi*(x+h/2)))/(L+1)
-        valKappaNeg += (np.sin(L*np.pi*(x-h/2)))/(L+1)
-    kappaPos.append(valKappaPos)
-    kappaNeg.append(valKappaNeg)
+# setting kappa as a constant to debug
+kappa = 4
+
+# kappaPos = []
+# kappaNeg = []
+# for x in xVec:
+#     valKappaPos = 2
+#     valKappaNeg = 2
+#     for L in range(0,6):
+#         valKappaPos += (np.sin(L*np.pi*(x+h/2)))/(L+1)
+#         valKappaNeg += (np.sin(L*np.pi*(x-h/2)))/(L+1)
+#     kappaPos.append(valKappaPos)
+#     kappaNeg.append(valKappaNeg)
 
 
 fApprox = []
-for int in j:
-    val = -(int*(int-1))*(kappaPos[int]+kappaNeg[int])+(kappaPos[int]*((int-h)*
-    (int+h-1)))+(kappaNeg[int]*((int-h)*(int-h-1)))
+for int in xVec:
+    val = -(int*(int-1))*(kappa+kappa)+(kappa*((int-h)*
+    (int+h-1)))+(kappa*((int-h)*(int-h-1)))
     fApprox.append(val/(h**2))
 
 
-print(abs(norm(fApprox)-norm(fVec)))
-# print(fApprox,fVec)
+print(fApprox,fVec)
