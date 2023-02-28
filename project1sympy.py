093165a224d79938be8952e5a592a91b434c0ca7
pmalen327 <malenpreston@gmail.com>
2023-02-28 08:37:56 -0700

    turns out kappa is the issue, working on kappaPos and kappaNeg

--- project1sympy.py
+++ project1sympy.py
@@ -7,6 +7,7 @@ from numpy import linalg as la
 from scipy.linalg import norm
 
 
+
 N = 10
 h = 1/(N+1)
 j = []
@@ -18,7 +19,6 @@ for int in j:
     xVec.append(int*h)
 
 
-# THIS IS THE PROBLEM
 fVec = []
 for x in xVec:
     val1 = 8
@@ -26,28 +26,30 @@ for x in xVec:
     val3 = 0
     for L in range(1,6):
         val2 += 2*(np.sin(np.radians(L*np.pi*x)))/(L+1)
-        val3 += ((2*x-4)*(np.cos(np.radians(np.pi*L*x)))*L*np.pi*x)/(L+1)
+        val3 += ((2*x-4)*(np.cos(np.radians(np.pi*L*x)))*np.radians(L*np.pi*x))/(L+1)
     fVec.append(val1+val2+val3)
 
-# setting kappa as a constant to debug
-kappa = 4
 
-# kappaPos = []
-# kappaNeg = []
-# for x in xVec:
-#     valKappaPos = 2
-#     valKappaNeg = 2
-#     for L in range(0,6):
-#         valKappaPos += (np.sin(L*np.pi*(x+h/2)))/(L+1)
-#         valKappaNeg += (np.sin(L*np.pi*(x-h/2)))/(L+1)
-#     kappaPos.append(valKappaPos)
-#     kappaNeg.append(valKappaNeg)
 
+# THIS IS THE PROBLEM
+kappaPos = []
+kappaNeg = []
+for x in xVec:
+    valKappaPos = 2
+    valKappaNeg = 2
+    for L in range(1,6):
+        valKappaPos += (np.sin(np.radians(L*np.pi*(x+h/2))))/(L+1)
+        valKappaNeg += (np.sin(np.radians(L*np.pi*(x-h/2))))/(L+1)
+    kappaPos.append(valKappaPos)
+    kappaNeg.append(valKappaNeg)
 
 fApprox = []
-for int in xVec:
-    val = -(int*(int-1))*(kappa+kappa)+(kappa*((int-h)*
-    (int+h-1)))+(kappa*((int-h)*(int-h-1)))
+for index in range(0, len(xVec)):
+    val = 0
+    uInit = (index*(x-1))
+    uNeg = (index-h)*(index-h-1)
+    uPos = (index+h)*(index+h-1)
+    val += (-uInit*(kappaPos[index]+kappaNeg[index])) + (kappaPos[index]*uPos) + (kappaNeg[index]*uNeg)
     fApprox.append(val/(h**2))
 
 
