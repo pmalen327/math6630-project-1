fa8f1c32ceae332ac8c6d504c32b191f96fed31a
pmalen327 <malenpreston@gmail.com>
2023-03-01 10:11:38 -0700

    now I'm having issues with fApprox again, I feel like I'm going in circle

--- project1sympy.py
+++ project1sympy.py
@@ -3,61 +3,69 @@
 # February 2023
 
 import numpy as np
-from numpy import linalg as la
 from scipy.linalg import norm
+import matplotlib.pyplot as plt
 
+# Nvec = []
+# errorVec = []
+# for N in range(10, 60, 10):
 
+#     Nvec.append(N)
 
-N = 10
+N = 20
 h = round(1/(N+1),5)
 j = []
 for int in range(0,N+2):
     j.append(int)
-
 xVec = []
 for int in j:
     xVec.append(round(int*h,5))
 
-# THIS IS THE PROBLEM
-# fukkkkkkkkkkkkkkkkk
 fVec = []
 for x in xVec:
-    val1 = 8
-    val2 = 0
-    val3 = 0
+    sum = 4
     for L in range(1,6):
-        val2 += 2*(np.sin(L*np.pi*x))/(L+1)
-        val3 += ((2*x-4)*(np.cos(np.pi*L*x))*L*np.pi*x)/(L+1)
-    fVec.append(val1+val2+val3)
-
-
-
-# kappaPos = [2 for i in range(0,len(xVec))]
-# kappaNeg = [3 for i in range(0,len(xVec))]
-
+        sum += ((2*np.sin(L*np.pi*x))+((2*x-1)*np.cos(L*np.pi*x)*L*np.pi*x))/(L+1)
+    fVec.append(round(sum,5))
+    
 kappaPos = []
 kappaNeg = []
-
-
 for x in xVec:
     valKappaPos = 2
     valKappaNeg = 2
     for L in range(1,6):
         valKappaPos += (np.sin(L*np.pi*(x+h/2)))/(L+1)
-        valKappaNeg += (np.sin(L*np.pi*(x+h/2)))/(L+1)
+        valKappaNeg += (np.sin(L*np.pi*(x-h/2)))/(L+1)
     kappaPos.append(valKappaPos)
     kappaNeg.append(valKappaNeg)
-
 
 fApprox = []
 for index in range(0, len(xVec)):
     val = 0
-    uInit = (index*(index-1))
-    uNeg = (index-h)*(index-h-1)
-    uPos = (index+h)*(index+h-1)
-    val += -(uInit*(kappaPos[index]+kappaNeg[index])) + (kappaPos[index]*uPos) + (kappaNeg[index]*uNeg)
-    fApprox.append(round(val/(h**2),5))
+    uInit = (xVec[index]*(xVec[index]-1))
+    uNeg = (xVec[index]-h)*(xVec[index]-h-1)
+    uPos = (xVec[index]+h)*(xVec[index]+h-1)
+    val += (-(uInit*(kappaPos[index]+kappaNeg[index])) + (kappaPos[index]*uPos)
+             + (kappaNeg[index]*uNeg))/(h**2)
+    fApprox.append(round(val,5))
 
-
-# print(fApprox,fVec)
 print(fVec,fApprox)
+
+# LTE = []
+# for i in range(0,len(fVec)):
+#     LTE.append(fApprox[i]-fVec[i])
+# errorVec.append(norm(LTE))
+
+# print(Nvec,errorVec)
+
+# we want to look at the norm of the LTE as N grows
+# fig,ax = plt.subplots()
+# ax.plot(Nvec,errorVec)
+# ax.set(xlabel='N', ylabel='Local Truncation Error')
+# plt.yscale('log')
+# plt.show()
+
+
+
+
+
